857ea86aa212af7d0ca578418edc2a5e338cb022
AnastasyMeleshko <7634907@mail.ru>
2023-09-01 19:02:28 +0300

    US-35: add service worker

--- src/repository.ts
+++ src/repository.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const repository = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true,
+  withCredentials: false,
 });
 
 repository.interceptors.request.use((config) => {
---
+++ src/themes.ts
@@ -0,0 +1,5 @@
+import { createTheme } from '@mui/material';
+
+const themes = createTheme({});
+
+export default themes;
