22b3ebd21615f13a99c568e2b34fdc191e6b1de5
Dmitry Kudryavcev <dmitrukudryavtsev@gmail.com>
2023-09-10 13:20:17 +0300

    US-43:2 add product search logic

--- src/app/products/store/products.selectors.ts
+++ src/app/products/store/products.selectors.ts
@@ -2,3 +2,5 @@ import { RootState } from '../../../store';
 
 export const productsSelector = (state: RootState) => state.products.products;
 export const productsPendingSelector = (state: RootState) => state.products.pending;
+
+export const foundproductsSelector = (state: RootState) => state.products.foundProducts;
